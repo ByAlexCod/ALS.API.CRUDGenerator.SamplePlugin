5a6b057bd24b55f7bf591bff13ce06b558789cd8
Alex S <alex.spitz@miuros.com>
2020-07-01 00:38:30 +0200

    Sample of plugin v1

---
+++ ALS.API.CRUDGenerator.SamplePlugin/MainClass.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ALS.API.CRUDGenerator.SamplePlugin
+{
+    public class MainClass
+    {
+        public static void Start(string jsonConfig, string modelInfos)
+        {
+            // Do what ever you need to with the parameters
+            Console.WriteLine(jsonConfig + modelInfos);
+        }
+    }
+}
